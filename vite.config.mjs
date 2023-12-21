b6cb1f02549324e672196282efb94617b97ad73e
dpi <pro@danielph.in>
2023-12-21 16:00:27 +0800

    Remove rollupOptions

--- vite.config.mjs
+++ vite.config.mjs
@@ -21,6 +21,18 @@ export default defineConfig({
       fileName: 'toasty',
       formats: ["es"],
     },
+    // rollupOptions: {
+    //   // make sure to externalize deps that shouldn't be bundled
+    //   // into your library
+    //   external: ['vue'],
+    //   output: {
+    //     // Provide global variables to use in the UMD build
+    //     // for externalized deps
+    //     globals: {
+    //       vue: 'Vue',
+    //     },
+    //   },
+    // },
   },
   target: "es2015",
   resolve: {
